505a98a29adc5299bfef3a0a176cdb13ed966117
Umar13755 <umarkhayyam570@gmail.com>
2024-10-06 08:19:52 +0500

    Services Updated

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,15 +1,25 @@
+import Card from "./ui/Card";
+import { NotebookIcon } from "lucide-react";
 export default function Services() {
   return (
     <>
-      <div className="text-center my-20 ">
-        <h2 className="heading2">Services</h2>
-      </div>
-
-      <div className="grid grid-cols-3">
-        <div>Card 1</div>
-        <div>Card 2</div>
-        <div>Card 3</div>
-      </div>
+      <section className="">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex justify-center gap-3 mb-4">
+            <NotebookIcon size={38} color="#ef4444" />
+            <h2 className="heading2">Services</h2>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <Card
+              title="Viral Content Strategies"
+              description="Fingerstache pinterest occupy wayfarers pickled semiotics everyday carry."
+              linkText="Find Out"
+              linkHref="#"
+              imageSrc="https://plus.unsplash.com/premium_photo-1663100543409-061876b76b0e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,52 @@
+interface ServiceCardProps {
+  title: string;
+  description: string;
+  linkText: string;
+  linkHref: string;
+  imageSrc: string; 
+}
+
+const Card: React.FC<ServiceCardProps> = ({
+  title,
+  description,
+  linkText,
+  linkHref,
+  imageSrc,
+}) => {
+  return (
+    <div className="p-4 lg:w-1/3">
+      <div className="h-full bg-white bg-opacity-10 backdrop-blur-lg shadow-lg  rounded-lg overflow-hidden relative">
+        {/* Image */}
+        <img
+          src={imageSrc}
+          alt="image"
+          className="w-full h-48 object-cover rounded-t-lg mb-4"
+        />
+        {/* Glassmorphism effect */}
+        <div className="px-8 py-5">
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-white mb-3">
+            {title}
+          </h1>
+          <p className="leading-relaxed mb-3 opacity-80">{description}</p>
+          <a href={linkHref} className="text-red-500 inline-flex items-center">
+            {linkText}
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
