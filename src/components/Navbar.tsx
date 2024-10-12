4f3f62b6bc16afca7343d7ef5261f4f318ca07c5
Umar13755 <umarkhayyam570@gmail.com>
2024-10-12 20:59:49 +0500

    Navbar Mobile Menu updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,58 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./ui/Button";
+import { AlignCenter } from "lucide-react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const MobileMenu = () => {
+    return (
+      <div className="absolute top-16 left-0 right-0 w-full py-20 bg-opacity-10 backdrop-blur-lg bg-[#ededed] z-50 text-white flex flex-col justify-center items-center space-y-6">
+        <nav className="flex flex-col items-center space-y-6">
+          <a
+            href="#"
+            className="text-lg font-semibold  hover:text-blue-600 transition duration-300"
+          >
+            Services
+          </a>
+          <a
+            href="#"
+            className="text-lg font-semibold  hover:text-blue-600 transition duration-300"
+          >
+            About Us
+          </a>
+          <a
+            href="#"
+            className="text-lg font-semibold  hover:text-blue-600 transition duration-300"
+          >
+            Success Stories
+          </a>
+          <a
+            href="/contact"
+            className="text-lg font-semibold  hover:text-blue-600 transition duration-300"
+          >
+            Contact Us
+          </a>
+        </nav>
+
+        <button className="px-6 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition duration-300">
+          Book Appointment
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <nav className="flex flex-row  justify-between items-center ">
-        <img className="w-24 h-auto " src="/assets/logo.png" alt="logo" />
+      <nav className="flex flex-row z-0 justify-between items-center ">
+        <img className="w-24 h-auto" src="/assets/logo.png" alt="logo" />
 
         <div className="gap-7 lg:flex hidden items-center">
           <Link href="#" className="link">
@@ -27,7 +73,9 @@ export default function Navbar() {
           <Button href="/booking" text="Contact Us" />
         </div>
 
-        <div className="lg:hidden flex">Humurger Menu</div>
+        <div className="lg:hidden flex" onClick={handleMenu}>
+          {isOpen ? <MobileMenu /> : <AlignCenter />}
+        </div>
       </nav>
     </div>
   );
